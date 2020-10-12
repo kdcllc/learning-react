94cf203afa510e462bfc4135cad083c018c7bce2
kdcllc <info@kingdavidconsulting.com>
2020-10-12 08:05:07 -0400

    using promises for loading from apis

--- src/learning-react-shop/src/App.jsx
+++ src/learning-react-shop/src/App.jsx
@@ -1,51 +1,24 @@
 import React from "react";
+import { useEffect } from 'react';
 import { useState } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-
-const products = [
-  {
-    id: 1,
-    category: "shoes",
-    image: "shoe1.jpg",
-    name: "Hiker",
-    price: 94.95,
-    skus: [
-      { sku: "17", size: 7 },
-      { sku: "18", size: 8 },
-    ],
-    description: "This rugged boot will get you up the mountain safely.",
-  },
-  {
-    id: 2,
-    category: "shoes",
-    image: "shoe2.jpg",
-    name: "Climber",
-    price: 78.99,
-    skus: [
-      { sku: "28", size: 8 },
-      { sku: "29", size: 9 },
-    ],
-    description: "Sure-footed traction in slippery conditions.",
-  },
-  {
-    id: 3,
-    category: "shoes",
-    image: "shoe3.jpg",
-    name: "Explorer",
-    price: 145.95,
-    skus: [
-      { sku: "37", size: 7 },
-      { sku: "38", size: 8 },
-      { sku: "39", size: 9 },
-    ],
-    description: "Look stylish while stomping in the mud.",
-  },
-];
+import { getProducts } from './services/productService';
+import Spinner from './Spinner';
 
 export default function App() {
   const [size, setSize] = useState("");
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() =>{
+    getProducts("shoes")
+      .then((response) => setProducts(response))
+      .catch((e) => setError(e))
+      .finally(() => setLoading(false));
+  }, []);
 
   function rednderProduct(p) {
     return (
@@ -61,6 +34,9 @@ export default function App() {
   const filteredProducts = size
     ? products.filter((p) => p.skus.find((s) => s.size === parseInt(size)))
     : products;
+
+  if (error) throw error
+  if(loading) return <Spinner/>;
 
   return (
     <>
@@ -81,9 +57,7 @@ export default function App() {
             </select>
             {size && <h2> Found {filteredProducts.length} items</h2>}
           </section>
-          <section id='products'>
-            {filteredProducts.map(rednderProduct)}
-          </section>
+          <section id='products'>{filteredProducts.map(rednderProduct)}</section>
         </main>
       </div>
       <Footer />
